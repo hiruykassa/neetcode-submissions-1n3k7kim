9450a9daeaaa928d1aca35c5e9b4786cad614dca
Hiruy Kassa <hiruygirmak@gmail.com>
2026-04-08 14:21:25 -0500

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while(r < len(prices)):
+            if(prices[r] > prices[l]):
+                max_res = prices[r] - prices[l]
+                maxP = max(maxP, max_res)
+            else:
+                l = r
+            r += 1
+        
+        return maxP
+
+            
+
+
+
+
+
